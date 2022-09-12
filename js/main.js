bb99369e2830e6c4d224aa01ea72d3c41426aa93
ccavad <ccavad2015@gmail.com>
2022-09-12 19:03:27 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// swiper 
+let swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
