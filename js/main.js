03739688987e28e921aa347d5fce03a28d38d060
ccavad <ccavad2015@gmail.com>
2022-09-14 13:43:09 +0400

    homepage added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+// dom elements 
+
+const scrollTopBtn = document.querySelector(".scrollTopBtn")
+
 // swiper 
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".banner-slider .swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -8,3 +12,32 @@ let swiper = new Swiper(".mySwiper", {
     delay: 3000,
   },
 });
+
+var swiper2 = new Swiper(".partners-slider", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+// scroll
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollTopBtn.style.display = "block"
+  } else {
+    scrollTopBtn.style.display = "none"
+  }
+})
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+})
