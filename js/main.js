0c6c090d36f5087e031f25b25775e19c5a07ced1
ccavad <ccavad2015@gmail.com>
2022-09-18 20:26:19 +0400

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-// dom elements 
+// dom elements
 
-const scrollTopBtn = document.querySelector(".scrollTopBtn")
+const scrollTopBtn = document.querySelector(".scrollTopBtn");
+const products = document.querySelectorAll(".product");
+
+// variables 
+
+let cartData = [];
+
+// cart functionality 
+
+products.forEach(product => {
+  product.addEventListener("click", e => {
+    if (e.target.classList.contains("addToCartBtn")) {
+      const prd = e.target.parentNode;
+      console.log(prd.querySelector(".product-price").innerText)
+    } else if (e.target.parentNode.classList.contains("product-wished")) {
+      e.target.src = "assets/images/icon/heart-filled.svg";
+    }
+  })
+})
 
 // swiper 
 let swiper = new Swiper(".banner-slider .swiper", {
@@ -23,6 +41,21 @@ var swiper2 = new Swiper(".partners-slider", {
   autoplay: {
     delay: 3000,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 25
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
 });
 
 // scroll
